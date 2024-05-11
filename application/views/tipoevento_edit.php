359f2496976d6880e51b51d23ca9c3b968d8ebf2
JannerG <estiven.guagua1@gmail.com>
2024-05-10 20:04:43 -0500

    up

--- application/views/tipoevento_edit.php
+++ application/views/tipoevento_edit.php
@@ -2,26 +2,28 @@
 <?php echo form_hidden('idtipoevento',$tipoevento['idtipoevento']) ?>
 <h2> <?php echo $title; ?></h2>
 <hr />
-<table>
- 
-   <tr>
-     <td>Id tipoevento</td>
-     <td><?php 
+
+  <div class="form-group row">
+    <label class="col-md-2 col-form-label"> Id tipoevento:</label>
+	<div class="col-md-10">
+	<?php
+      echo form_input('idtipoevento',$tipoevento['idtipoevento'],array('placeholder'=>'Idtipoevento')); ?>
+	</div> 
+  </div> 
 
 
-$eys_arrinput=array('name'=>'idtipoevento','value'=>$tipoevento['idtipoevento'],'readonly'=>'true', "style"=>"width:500px");
-echo form_input($eys_arrinput); ?></td>
-  </tr> 
-  <tr>
-      <td>Nombre:</td>
-      <td><?php
- 
-$eys_arrinput=array('name'=>'nombre','value'=>$tipoevento['nombre'], "style"=>"width:500px");
- echo form_input($eys_arrinput); ?></td>
-  </tr>
- 
+  <div class="form-group row">
+    <label class="col-md-2 col-form-label"> Nombre:</label>
+	<div class="col-md-10">
+	<?php
+       echo form_input('nombre',$tipoevento['nombre'],array('placeholder'=>'Nombre tipoevento')); ?>
+	</div> 
+  </div> 
+
+
+  <table> 
  <tr>
  <td colspan="2"> <hr><?php echo form_submit('submit', 'Guardar'); ?> <?php echo anchor('tipoevento','Atras') ?></td>
  </tr>
-</table>
-<?php echo form_close(); ?>
+  </table>
+  <?php echo form_close(); ?>
