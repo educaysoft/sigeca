c7552ade19ed2e563c7ed5952c6de508e4e0f57f
Danna <isaacgonzalezgs2003@gmail.com>
2024-05-13 19:54:25 -0500

    "Mejoras ActividadB2-1"

--- application/views/estadocivil_list.php
+++ application/views/estadocivil_list.php
@@ -3,32 +3,32 @@
 
 <body>
 
-<h2> <?php echo $title;  ?></h2>
-<hr/>
-<?php echo anchor('estadocivil/add', 'NUEVO ORDENADOR'); ?>
-<br>
-<br>
+    <h2> <?php echo $title;  ?></h2>
+    <hr />
+    <?php echo anchor('estadocivil/add', 'NUEVO ORDENADOR'); ?>
+    <br>
+    <br>
 
-<table border="1">
-<tr>
-<th> ID ORDENADOR</th>
-<th> DESCRIPCION</th>
-<th> cantidad</th>
-</tr>
-<tbody>
-<?php  foreach($estadocivil_list as $list) { ?>
-<tr>
+    <table border="1">
+        <tr>
+            <th> ID ORDENADOR</th>
+            <th> DESCRIPCION</th>
+            <th> cantidad</th>
+        </tr>
+        <tbody>
+            <?php foreach ($estadocivil_list as $list) { ?>
+                <tr>
 
-<td> <?php echo $list->idestadocivil ?></td>
-<td> <?php echo $list->nombre?></td>
-<td> <?php echo anchor('documento/listarxestadocivil/'.$list->idestadocivil,'ver') ?> || <?php echo anchor('documento/add/'.$list->idestadocivil,'Nuevo') ?></td>
- </tr>
+                    <td> <?php echo $list->idestadocivil ?></td>
+                    <td> <?php echo $list->nombre ?></td>
+                    <td> <?php echo anchor('documento/listarxestadocivil/' . $list->idestadocivil, 'ver') ?> || <?php echo anchor('documento/add/' . $list->idestadocivil, 'Nuevo') ?></td>
+                </tr>
 
 
-<?php } ?>
+            <?php } ?>
 
-</tbody>
-</table>
+        </tbody>
+    </table>
 
 
 
