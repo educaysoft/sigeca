f0d2dd0faa0be7e0f122bf36a4db791f784c8e0f
Jose Solis <jose.solisplaza@gmail.com>
2024-05-14 07:26:16 -0500

    Mejoras en el diseño

--- application/views/modoevaluacion_record.php
+++ application/views/modoevaluacion_record.php
@@ -5,7 +5,7 @@
 <!--Fecha: Ultima evaluación: Sabado 4 febrero 2023 -->
 
 <div id="eys-nav-i" class="mt-4 p-2">
-    <h3 class="mt-3 pt-3"><?php echo $title; ?></h3>
+    <h3 class="mt-3 p-3"><?php echo $title; ?></h3>
     <ul class="list-inline d-inline">
         <?php if (isset($modoevaluacion) && isset($modoevaluacion['idmodoevaluacion'])) : ?>
             <li class="list-inline-item"> <?php echo anchor('modoevaluacion/elprimero/', 'Primero'); ?></li>
@@ -15,15 +15,21 @@
             <li class="list-inline-item"> <?php echo anchor('modoevaluacion/add', 'Nuevo'); ?></li>
             <li class="list-inline-item"> <?php echo anchor('modoevaluacion/edit/' . $modoevaluacion['idmodoevaluacion'], 'Edit'); ?></li>
             <li class="list-inline-item"> <?php echo anchor('modoevaluacion/delete/' . $modoevaluacion['idmodoevaluacion'], 'Delete'); ?></li>
-            <li class="list-inline-item"> <?php echo anchor('modoevaluacion/listar/', 'Listar'); ?></li>
+            <li class="list-inline-item" style="border-left: 1px solid #ccc; padding-left: 10px;"> <?php echo anchor('modoevaluacion/listar/', 'Listar'); ?></li>
         <?php endif; ?>
     </ul>
 </div>
 
 <?php echo form_open('modoevaluacion/save_edit') ?>
-<?php if (isset($modoevaluacion['idmodoevaluacion'])) : ?>
-    <?php echo form_hidden('idmodoevaluacion', $modoevaluacion['idmodoevaluacion']); ?>
-<?php endif; ?>
+
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Id:</label>
+    <div class="col-md-10">
+        <?php if (isset($modoevaluacion['idmodoevaluacion'])) : ?>
+            <?php echo form_hidden('idmodoevaluacion', $modoevaluacion['idmodoevaluacion']); ?>
+        <?php endif; ?>
+    </div>
+</div>
 
 <div class="form-group row">
     <label class="col-md-2 col-form-label"> Id:</label>
