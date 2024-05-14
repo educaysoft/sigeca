6ed467afc0bf0e9f09d36999016daf06b2cde845
JannerG <estiven.guagua1@gmail.com>
2024-05-14 00:22:47 -0500

    sads

--- application/models/Tipoevento_model.php
+++ application/models/Tipoevento_model.php
@@ -73,10 +73,7 @@ class Tipoevento_model extends CI_model {
 				$this->db->where('idtipoevento',$id);
 			 $this->db->update('tipoevento', array('eliminado'=>1));
 			 $result=true;
-		 }else{
-			 $result=false;
 		 }
-		 return $result;
 	  }
  
 
