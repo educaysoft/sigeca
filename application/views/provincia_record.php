97db45c0923d7851892d0936f921d6914a553fc0
Edieded <eddy.garcia@utelvt.edu.ec>
2024-05-16 12:24:14 -0500

    mejoras 3

--- application/views/provincia_record.php
+++ application/views/provincia_record.php
@@ -1,42 +1,34 @@
 <div id="eys-nav-i">
-
-<h3 style="text-align: left; margin-top:-10px;"> <?php echo $title;  ?></h3>
-<?php echo form_open('provincia/save_edit') ?>
+    <h3 style="text-align: left; margin-top:-10px;"><?php echo $title; ?></h3>
+    <?php echo form_open('provincia/save_edit') ?>
     <ul>
-        <li> <?php echo anchor('provincia/elprimero/', 'primero'); ?></li>
-        <?php if(isset($provincia['idprovincia'])): ?>
-        <li> <?php echo anchor('provincia/anterior/'.$provincia['idprovincia'], 'anterior'); ?></li>
-        <li> <?php echo anchor('provincia/siguiente/'.$provincia['idprovincia'], 'siguiente'); ?></li>
-        <li> <?php echo anchor('provincia/edit/'.$provincia['idprovincia'],'Edit'); ?></li>
-        <li style="border-right:1px solid green"> <?php echo anchor('provincia/delete/'.$provincia['idprovincia'],'Delete'); ?></li>
+        <li><?php echo anchor('provincia/elprimero/', 'primero'); ?></li>
+        <?php if (isset($provincia['idprovincia'])): ?>
+            <li><?php echo anchor('provincia/anterior/' . $provincia['idprovincia'], 'anterior'); ?></li>
+            <li><?php echo anchor('provincia/siguiente/' . $provincia['idprovincia'], 'siguiente'); ?></li>
+            <li><?php echo anchor('provincia/edit/' . $provincia['idprovincia'], 'Edit'); ?></li>
+            <li style="border-right:1px solid green"><?php echo anchor('provincia/delete/' . $provincia['idprovincia'], 'Delete'); ?></li>
         <?php endif; ?>
-        <li> <?php echo anchor('provincia/elultimo/', 'Último'); ?></li>
-        <li> <?php echo anchor('provincia/add', 'Nuevo'); ?></li>
-        <li> <?php echo anchor('provincia/listar/','Listar'); ?></li>
+        <li><?php echo anchor('provincia/elultimo/', 'Último'); ?></li>
+        <li><?php echo anchor('provincia/add', 'Nuevo'); ?></li>
+        <li><?php echo anchor('provincia/listar/', 'Listar'); ?></li>
     </ul>
 </div>
 <br>
 
-<?php echo form_hidden('idprovincia', isset ($provincia['idprovincia']) ? $provincia['idprovincia'] : '') ?>
+<?php echo form_hidden('idprovincia', isset($provincia['idprovincia']) ? $provincia['idprovincia'] : ''); ?>
 <table>
-
- 
- 
-
-  <tr>
-     <td>Id Tipo Doc:</td>
-     <td><?php echo form_input('idprovincia',isset ($provincia['idprovincia']) ? $provincia['idprovincia'] : '',array("disabled"=>"disabled",'placeholder'=>'Idprovincias')) ?></td>
-  </tr>
- 
- 
- 
-  <tr>
-     <td>Descripción:</td>
-     <td><?php echo form_input('nombre',isset ($provincia['nombre']) ? $provincia ['nombre'] ,array("disabled"=>"disabled",'placeholder'=>'nombre')) ?></td>
-  </tr>
-
+    <tr>
+        <td>Id Tipo Doc:</td>
+        <td><?php echo form_input('idprovincia', isset($provincia['idprovincia']) ? $provincia['idprovincia'] : '', array("disabled" => "disabled", 'placeholder' => 'Idprovincias')); ?></td>
+    </tr>
+    <tr>
+        <td>Descripción:</td>
+        <td><?php echo form_input('nombre', isset($provincia['nombre']) ? $provincia['nombre'] : '', array("disabled" => "disabled", 'placeholder' => 'nombre')); ?></td>
+    </tr>
 </table>
 <?php echo form_close(); ?>
 </body>
 </html>
+
 
