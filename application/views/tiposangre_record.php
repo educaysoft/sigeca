083f84a2e668691dfea555350410000d2eddd6eb
jcoime8 <jcoime8@gmail.com>
2024-05-16 14:07:47 -0500

    Probando posibles soluciones

--- application/views/tiposangre_record.php
+++ application/views/tiposangre_record.php
@@ -4,14 +4,13 @@
 <?php echo form_open('tiposangre/save_edit') ?>
     <ul>
         <li> <?php echo anchor('tiposangre/elprimero/', 'primero'); ?></li>
-        <li> <?php echo anchor('tiposangre/anterior/'.$tiposangre['idtiposangre'], 'anterior'); ?></li>
-        <li> <?php echo anchor('tiposangre/siguiente/'.$tiposangre['idtiposangre'], 'siguiente'); ?></li>
+        <li> <?php echo anchor('tiposangre/anterior/'.(isset($tiposangre['idtiposangre']) ? $tiposangre['idtiposangre'] : ''), 'anterior'); ?></li>
+        <li> <?php echo anchor('tiposangre/siguiente/'.(isset($tiposangre['idtiposangre']) ? $tiposangre['idtiposangre'] : ''), 'siguiente'); ?></li>
         <li style="border-right:1px solid green"><?php echo anchor('tiposangre/elultimo/', 'Último'); ?></li>
         <li> <?php echo anchor('tiposangre/add', 'Nuevo'); ?></li>
-        <li> <?php echo anchor('tiposangre/edit/'.$tiposangre['idtiposangre'],'Edit'); ?></li>
-        <li style="border-right:1px solid green"> <?php echo anchor('tiposangre/delete/'.$tiposangre['idtiposangre'],'Delete'); ?></li>
+        <li> <?php echo anchor('tiposangre/edit/'.(isset($tiposangre['idtiposangre']) ? $tiposangre['idtiposangre'] : ''),'Edit'); ?></li>
+        <li style="border-right:1px solid green"> <?php echo anchor('tiposangre/delete/'.(isset($tiposangre['idtiposangre']) ? $tiposangre['idtiposangre'] : ''),'Delete'); ?></li>
         <li> <?php echo anchor('tiposangre/listar/','Listar'); ?></li>
-
     </ul>
 </div>
 <br>
@@ -20,33 +19,15 @@
 <?php echo form_hidden('idtiposangre',$tiposangre['idtiposangre']) ?>
 <table>
 
-
- 
- 
-
-
   <tr>
      <td>Id Tipo Doc:</td>
      <td><?php echo form_input('idtiposangre',$tiposangre['idtiposangre'],array("disabled"=>"disabled",'placeholder'=>'Idtiposangres')) ?></td>
   </tr>
  
- 
- 
   <tr>
      <td>Descripción:</td>
      <td><?php echo form_input('nombre',$tiposangre['nombre'],array("disabled"=>"disabled",'placeholder'=>'nombre')) ?></td>
   </tr>
-
-
-  
-
-
-
-
-
-
-
-
 </table>
 <?php echo form_close(); ?>
 
