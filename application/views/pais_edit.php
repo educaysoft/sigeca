b5a2522b82d37b79b47f12aa6b09ca0484781851
Josselyn Bombon Gomez <josselyn.bombon.gomez@utelvt.edu.ec>
2024-05-16 22:20:40 -0500

    Mejoras

--- application/views/pais_edit.php
+++ application/views/pais_edit.php
@@ -1,24 +1,36 @@
-<?php echo form_open('pais/save_edit', array('class' => 'form-horizontal')); ?>
-<?php echo form_hidden('idpais', $pais['idpais']); ?>
+<?php echo form_open('pais/save_edit', array('class' => 'form-horizontal')) ?>
+<?php echo form_hidden('idpais', $pais['idpais']) ?>
 <h2><?php echo $title; ?></h2>
 <hr />
 
-<div class="container">
-  <div class="row">
-    <div class="col-md-6">
-      <div class="form-group">
-        <label for="idpais" class="control-label">Id país:</label>
-        <?php echo form_input(array('name' => 'idpais', 'value' => $pais['idpais'], 'class' => 'form-control', 'placeholder' => 'Id país')); ?>
-      </div>
-      <div class="form-group">
-        <label for="nombre" class="control-label">Nombre:</label>
-        <?php echo form_input(array('name' => 'nombre', 'value' => $pais['nombre'], 'class' => 'form-control', 'placeholder' => 'Nombre país')); ?>
-      </div>
-      <div class="form-group">
-        <?php echo form_submit('submit', 'Guardar', array('class' => 'btn btn-primary')); ?>
-        <?php echo anchor('pais', 'Atrás', array('class' => 'btn btn-secondary')); ?>
-      </div>
-    </div>
-  </div>
+<div class="table-responsive">
+  <table class="table table-bordered">
+    <tr>
+      <td>Id país</td>
+      <td>
+        <?php
+        $eys_arrinput = array('name' => 'idpais', 'value' => $pais['idpais'], 'readonly' => 'true', 'class' => 'form-control', 'style' => 'width: 200px;');
+        echo form_input($eys_arrinput);
+        ?>
+      </td>
+    </tr>
+    <tr>
+      <td>Nombre:</td>
+      <td>
+        <?php
+        $eys_arrinput = array('name' => 'nombre', 'value' => $pais['nombre'], 'class' => 'form-control', 'style' => 'width: 300px;');
+        echo form_input($eys_arrinput);
+        ?>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2">
+        <hr>
+        <?php echo form_submit('submit', 'Guardar', array('class' => 'btn btn-default')); ?>
+        <?php echo anchor('pais', 'Atrás', array('class' => 'btn btn-default')); ?>
+      </td>
+    </tr>
+  </table>
 </div>
+
 <?php echo form_close(); ?>
