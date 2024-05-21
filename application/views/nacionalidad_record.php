bf82b70afd3b7237e333a4c389b9b8e5ba35f4f7
Vanessa Vargas <101435741+VanessaVargas31@users.noreply.github.com>
2024-05-20 19:29:35 -0500

    He realizado los cambios del modulo nacionalidad

--- application/views/nacionalidad_record.php
+++ application/views/nacionalidad_record.php
@@ -1,7 +1,6 @@
 <div id="eys-nav-i">
-
-<h3 style="text-align: left; margin-top:-10px;"> <?php echo $title;  ?></h3>
-<?php echo form_open('nacionalidad/save_edit') ?>
+    <h3 style="text-align: left; margin-top:-10px;"> <?php echo $title;  ?></h3>
+    <?php echo form_open('nacionalidad/save_edit') ?>
     <ul>
         <li> <?php echo anchor('nacionalidad/elprimero/', 'primero'); ?></li>
         <li> <?php echo anchor('nacionalidad/anterior/'.$nacionalidad['idnacionalidad'], 'anterior'); ?></li>
@@ -11,57 +10,21 @@
         <li> <?php echo anchor('nacionalidad/edit/'.$nacionalidad['idnacionalidad'],'Edit'); ?></li>
         <li style="border-right:1px solid green"> <?php echo anchor('nacionalidad/delete/'.$nacionalidad['idnacionalidad'],'Delete'); ?></li>
         <li> <?php echo anchor('nacionalidad/listar/','Listar'); ?></li>
-
     </ul>
 </div>
 <br>
 
-
 <?php echo form_hidden('idnacionalidad',$nacionalidad['idnacionalidad']) ?>
-<table>
-
-
- 
- 
-
-
-  <tr>
-     <td>Id Tipo Doc:</td>
-     <td><?php echo form_input('idnacionalidad',$nacionalidad['idnacionalidad'],array("disabled"=>"disabled",'placeholder'=>'Idnacionalidads')) ?></td>
-  </tr>
- 
- 
- 
-  <tr>
-     <td>Descripción:</td>
-     <td><?php echo form_input('nombre',$nacionalidad['nombre'],array("disabled"=>"disabled",'placeholder'=>'nombre')) ?></td>
-  </tr>
-
-
-  
-
-
-
-
-
-
-
-
+<table style="border-collapse: collapse;">
+    <tr>
+        <td style="border: none;">Id Tipo Doc:</td>
+        <td style="border: none;"><?php echo form_input('idnacionalidad',$nacionalidad['idnacionalidad'],array("disabled"=>"disabled",'placeholder'=>'Idnacionalidads')) ?></td>
+    </tr>
+    <tr>
+        <td style="border: none;">Descripción:</td>
+        <td style="border: none;"><?php echo form_input('nombre',$nacionalidad['nombre'],array("disabled"=>"disabled",'placeholder'=>'nombre')) ?></td>
+    </tr>
 </table>
 <?php echo form_close(); ?>
-
-
-
-
-
 </body>
-
-
-
-
-
-
-
-
-
 </html>
