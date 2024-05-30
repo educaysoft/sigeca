475fe05f9311cea05f24334db7e7e907f37c275a
Stalin francis <educaysoft@gmail.com>
2024-05-30 15:37:17 -0500

    mejoras

--- application/controllers/Articulo.php
+++ application/controllers/Articulo.php
@@ -7,6 +7,7 @@ class Articulo extends CI_Controller{
       $this->load->model('articulo_model');
   	  $this->load->model('institucion_model');
   	  $this->load->model('ubicacionarticulo_model');
+  	  $this->load->model('prestamoarticulo_model');
 }
 
 public function index(){
